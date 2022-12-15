0ad78fb912edddce50648749aab472be68285ecb
TiberiuBadelita <badelita.tibi@yahoo.com>
2022-12-16 00:37:25 +0200

    Randomly-Spawned-Players

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -6,11 +6,17 @@ using Photon.Pun;
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
    public Transform playerSpawnPoint;
+   public GameObject XROrigin;
    private GameObject spawnedPlayerPrefab;
 
    public override void OnJoinedRoom()
    {
+       
         base.OnJoinedRoom();
+        int[ ] xSpawnPoints = new int[]{100, -100, 200} ;
+        int[] zSpawnPoints = new int[]{100, -100, -100};
+        var index = Random.Range(0, xSpawnPoints.Length);
+        XROrigin.transform.Translate(xSpawnPoints[index],0,zSpawnPoints[index]);
         spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", playerSpawnPoint.position, playerSpawnPoint.rotation);
    }
 
