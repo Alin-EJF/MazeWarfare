c1e3cd4ea4dc93ac92fa40da4dbf9d1f07ba7a0c
Alintdg <tdgalin@gmail.com>
2022-12-19 18:08:19 +0200

    locomotion system added

---
+++ Assets/CharacterMovementHelper.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
+
+public class CharacterMovementHelper : MonoBehaviour
+{
+
+    private XROrigin xROrigin;
+    private CharacterController characterController;
+    private CharacterControllerDriver driver;
+
+
+    void Start()
+    {
+        xROrigin = GetComponent<XROrigin>();
+        characterController = GetComponent<CharacterController>();
+        driver = GetComponent<CharacterControllerDriver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+
+    /// <summary>
+    /// Updates the <see cref="characterController.height"/> and <see cref="characterController.center"/>
+    /// based on the camera's position. 
+    /// </summary>
+
+    protected virtual void UpdateCharacterController()
+    {
+        if (xROrigin == null || characterController == null)
+            return;
+
+    var height = Mathf.Clamp(xROrigin.CameraInOriginSpaceHeight, driver.minHeight, driver.maxHeight);
+
+    Vector3 center = xROrigin.CameraInOriginSpacePos;
+    center.y = height / 2f + characterController.skinWidth;
+
+    characterController.height = height;
+    characterController.center = center;
+    }
+}
